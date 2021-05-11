d98b8580157efae3a62fe0a436d31ae9d37b843f
JericHunter <51177847+JericHunter@users.noreply.github.com>
2021-05-11 21:05:51 +0000

    .

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = require('./schema');
+
+const petList = [
+    { id: 1, name: 'Fluffy', species: 'Dog' },
+    { id: 2, name: 'Sassy', species: 'Cat' },
+    { id: 3, name: 'Goldberg', species: 'Frog' }
+];
+// Define a resolver
+const root = {
+    allPets: () => {
+        return petList;
+    },
+    getPet: ({ index }) => {
+        return petList[index];
+    },
+    firstPet: () => {
+        return petList[0];
+    },
+    addPet: ({ name, species }) => {
+        const pet = { name, species };
+        petList.push(pet);
+
+        return pet
+    },
+    updatePet: ({ id, name, species }) => {
+        const pet = petList[id];
+        if (pet === undefined) {
+            return null
+        }
+
+        pet.name = name || pet.name;
+        pet.species = species || pet.species;
+
+        return pet;
+    },
+    deletePet: ({ id }) => {
+        const pet = petList[id];
+
+        if (pet === undefined) {
+            return null;
+        }
+
+        petList.splice(id, 1);
+
+        return pet;
+    }
